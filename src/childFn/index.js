022e1b2ee573ffed5f8fab5474fa412794dcdd1c
Eric Laursen <ericjlaursen@gmail.com>
2019-01-19 12:23:02 -0500

    Updates fn output

--- src/childFn/index.js
+++ src/childFn/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import './index.css'
 
 //React.memo makes the fn skip re-rendering if props are the same
-const Child = React.memo((props) => {
-	console.log('Rendering child');
+const Child = React.memo(({thisCount, name}) => {
+	console.log(`Rendering ${name}`);
   return (
     <div className='frozenChild'>
       <p>This is a child fn component.</p>
       <p>This will not update unless the props are different.</p>
-      <p>This is a pureComponent.</p>
-      <p>This gets a prop value of {props.thisCount}</p>
+      <p>This gets a prop value of {thisCount}</p>
+      {(!name) && <p>This is a <b>pure function</b></p>}
     </div>
   );
 });
